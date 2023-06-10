6519bc6410d6ff1813eef299504b874da10da9e0
Moonstar5606 <77511557+Moonstar5606@users.noreply.github.com>
2023-06-10 10:49:00 -0400

    Add files via upload

---
+++ quizgame.py
@@ -0,0 +1,36 @@
+print("Welcome to my computer quiz! I hope you enjoy it. :)")
+playing=input("Do you want to play? ")
+if playing !="yes":
+    quit()
+print("Yay! Let's Play! :)")
+#Question 1
+answer=input("What does NASA stand for? ")
+if answer=="National Aeronautics and Space Administration":
+    print("Correct! Yay you got it! :)")
+else:
+    print("Aw~~~ You got it wrong. :,(")
+#Question 2
+answer=input("How old is the Universe?")
+if answer=="14 billion years":
+    print("That's right! The universe is said to be over 14 billion years old")
+else:
+    print("Well I'm glad you think that the universe is wiser or more youthful than it seems, but this is the age of the universe. Keep learning!")
+#Question 3
+answer=input("How far is 1 parsec?")
+if answer=="3.26 light years":
+    print("That's right! A parsec is one of the biggest astronomical units in astronomy. If you thought that a light year was big, imagine that times 3.26")
+else:
+    print("That is very understandable. Knowing the exact measurements of things is not something that people do often. Now you know> Great job! :D")
+#Question 4
+print("On that note, let's see if you know the unit for parsec!")
+answer=input("What is the astronomical unit for parsec?")
+if answer=="pc":
+    print("Correct! Just like meter is 'm', and light year is 'ly', parsec's unit is 'pc'. Great Job! :)")
+else:
+    print("That's understandable. pc for parsecs looks exactly like PC for computers. Isn't it so fun to learn? :D")
+#Question 5
+answer=input("So. Is astronomy an easy subject to learn?")
+if answer=="no":
+    print("That is very understandable. Astronomy takes a lot of knowledge of math. You'd have to do some form of calculus every single day. There is always more to learn. If you want to pursue astronomy or anything similar, I hope you do well and continue to learn your math! :)")
+if answer=="yes":
+    print("I'm glad you think so! Have fun on your journey and enjoy the ride, space ranger! :D")  
